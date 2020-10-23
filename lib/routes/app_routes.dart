31cd29f4e7f6429c6ce5bf7e2fadd17d0724ba4e
Marcus Duarte <marcusedu7@gmail.com>
2020-10-23 14:31:22 -0300

    Tela de historico registrada nas rotas

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:currency_conversion/di/injector.dart';
 import 'package:currency_conversion/ui/converter/converter_screen.dart';
+import 'package:currency_conversion/ui/history/history_screen.dart';
 import 'package:currency_conversion/ui/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,6 +16,9 @@ class AppRoutes {
           create: (_) => Injector.instance.converterCubit..loadRate(),
           child: ConverterScreen(),
         ),
-    // history: (context) => HistoryScreen()
+    history: (context) => BlocProvider(
+          child: HistoryScreen(),
+          create: (_) => Injector.instance.historyCubit..refresh(),
+        )
   };
 }
